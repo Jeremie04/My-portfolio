60db875f79b2e6c61447603811fa49b1cebc01ce
jeremie04 <rantojeremie@gmail.com>
2026-03-07 15:32:48 +0300

    fix: fix deployment

--- src/components/Services.tsx
+++ src/components/Services.tsx
@@ -1,5 +1,5 @@
 import { Brain, Code, Layers, Rocket } from "lucide-react";
-import { Card, CardContent, CardHeader, CardTitle } from "./ui/card";
+import { Card, CardContent } from "./ui/card";
 const services = [
   {
     title: "Conception & Architecture",
